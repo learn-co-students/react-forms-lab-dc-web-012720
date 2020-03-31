b4e44b5b1b40498ad4cb3cbddbcab09e795674a0
github username <laurenkyu@gmail.com>
2020-03-31 17:54:03 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,48 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
   }
+
+  handleInputChange = (event) => {
+    this.setState({[event.target.name]: event.target.value})
+  }
+
+  // handleUsernameChange = (event) => {
+  //   this.setState({username: event.target.value})
+  // }
+
+  // handlePasswordChange = (event) => {
+  //   this.setState({password: event.target.value})
+  // }
+
+  handleSubmit = (event) => 
+    {
+      event.preventDefault()
+  
+      if (!this.state.username || !this.state.password) {
+        return 
+        //this is returning nothing
+      }
+      this.props.handleLogin(this.state)
+    }
 
   render() {
     return (
-      <form>
+      <form onSubmit = {this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" value = {this.state.username} onChange = {this.handleInputChange}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" value = {this.state.password} onChange = {this.handleInputChange} />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,21 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ""
+    };
+  }
+
+  handleOnChange = (event) => {
+    this.setState({message: event.target.value})
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" value = {this.state.message} onChange = {this.handleOnChange}/>
+        <p>Remaining characters: {this.props.maxChars - this.state.message.length}</p>
       </div>
     );
   }
